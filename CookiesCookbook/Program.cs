28178bfe540dd63cca12b3345da536b0ff0b3274
Brando Lobo <brandogates@gmail.com>
2025-10-23 07:54:23 +0200

    Working on CookieCookbooks assignment for cookie recipes.

---
+++ CookiesCookbook/Program.cs
@@ -0,0 +1,40 @@
+﻿using CookiesCookbook.Models.BaseClasses;
+using CookiesCookbook.Utilities;
+
+var fileType = FileType.Json;
+
+GenerateSomeRecipes.GenerateAndWriteToFile(fileType);
+
+var recipes = GetRecipes();
+
+if (recipes != null)
+{
+    Console.WriteLine("Printing existing recipes.");
+    foreach (var recipe in recipes)
+    {
+        Console.WriteLine(recipe);
+    }
+}
+else
+    Console.WriteLine("No recipes to display.");
+
+Console.ReadKey();
+return;
+
+List<Recipe>? GetRecipes()
+{
+    try
+    {
+        List<Recipe>? savedRecipes = [];
+
+        if (fileType == FileType.Json)
+            savedRecipes = Serialization.DeserializeFromJson<List<Recipe>>();
+
+        return savedRecipes;
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+        throw;
+    }
+}
